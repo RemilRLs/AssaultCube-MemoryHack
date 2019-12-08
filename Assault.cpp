a292bd8d1cef28604f69c5d0c6e2dbfd3b9ec5d5
Remil <51848204+RemilRLs@users.noreply.github.com>
2019-12-08 03:11:42 +0100

    Main

--- Assault.cpp
+++ Assault.cpp
@@ -2,26 +2,6 @@
 #include <Windows.h>
 #include "MemoryWriteRead.h"
 
-/*
-    - On lui donne l'handle pour qu'il puisse se rattacher à la classe.
-    - On lui donne la baseadresse .
-    - On lui dit ou stocker la valeur, ici dans une structure.
-    - On lui dit la taille de la structure (sa sera toujours un int).
-
-    - player est un pointeur pointant sur le type playerData et donc sur la structure. (-> permet d'avoir accès à une structure).
-*/
-
-bool readDataPlayer(Bypass * handle, uintptr_t IpBaseAddress, playerData *player, SIZE_T nSize){ 
-    handle->Read((IpBaseAddress + offsets.player_health), &player->player_health, sizeof(player->player_health));
-    handle->Read((IpBaseAddress + offsets.player_kev), &player->player_kev, sizeof(player->player_kev));
-    handle->Read((IpBaseAddress + offsets.player_pos_x), &player->player_pos_x, sizeof(player->player_pos_x));
-    handle->Read((IpBaseAddress + offsets.player_pos_y), &player->player_pos_y, sizeof(player->player_pos_z));
-    handle->Read((IpBaseAddress + offsets.player_pos_z), &player->player_pos_z, sizeof(player->player_pos_z));
-    handle->Read((IpBaseAddress + offsets.player_pos_head), &player->player_pos_head, sizeof(player->player_pos_head));
-    handle->Read((IpBaseAddress + offsets.player_x_mouse), &player->player_x_mouse, sizeof(player->player_x_mouse));
-    handle->Read((IpBaseAddress + offsets.player_y_mouse), &player->player_y_mouse, sizeof(player->player_y_mouse));
-}
-
 
 
 int main(){
@@ -32,33 +12,23 @@ int main(){
     int godMod = 1000;
     int playerKev = 1000;
     int ammo = 20;
+    int ammoPistolGun = 10;
 
     std::cout << "[*] - ProcessID search..." << std::endl;
+    
     /*
         - FindWindowA va nous retourner un handle de la fenêtre (donc trouvé).
         Sinon FindWindowA nous retourne un NULL.
 
     */
-    HWND hwnd = FindWindowA(NULL, LPCSTR("AssaultCube"));
 
-    if (hwnd != NULL){
-        std::cout << "[*] - ProcessID find." << std::endl;
-    }
-    else{
-        std::cout << "[X] - Failure, we could not find the process" << std::endl;
-        system("pause");
-    }
-    GetWindowThreadProcessId(hwnd, &pid); // La variable pid va reçevoir le ProcessID.
-    if (pid != 0){
-        std::cout << "[*] - ProcessID find." << std::endl;
-    }
-    else{
-        std::cout << "[X] - No ProcessID finds." << std::endl;
-        system("pause");
-    }
 
     Bypass* bypass = new Bypass(); // On crée la classe.
-    if(!bypass->Attach(pid)){ // On génère les permissions souhaité sur le processus en question.
+    if(!bypass->findPID()){
+
+    }
+
+    if(!bypass->Attach()){ // On génère les permissions souhaité sur le processus en question.
         std::cout << "[X] - Exit." << std::endl;
     } 
 
@@ -85,7 +55,7 @@ int main(){
 
     while(!exit){
 
-        if(!readDataPlayer(bypass, (baseAddress), &player, sizeof(player))){
+        if(!bypass->readDataPlayer((baseAddress), &player)){
             std::cout << "[X] - We could not read the player's data." << std::endl;
         }
 
@@ -102,7 +72,7 @@ int main(){
             infinite_ammo = true;
         }
 
-        if(!readDataPlayer(bypass, (baseAddress), &player, sizeof(player))){
+        if(!bypass->readDataPlayer((baseAddress), &player)){
             std::cout << "[X] We could not recover the player's values.\n[X]Exiting..." << std::endl;
             break;
         }
@@ -115,6 +85,8 @@ int main(){
         }
         if(infinite_ammo){
             if(bypass->Write((baseAddress + offsets.player_ammo), &ammo, sizeof(ammo))){
+            }
+            if(bypass->Write((baseAddress + offsets.player_ammo_pistolgun), &ammoPistolGun, sizeof(ammoPistolGun))){
             }
         }
     }
--- MemoryWriteRead.h
+++ MemoryWriteRead.h
@@ -4,6 +4,7 @@
 struct playerOffset{
     uint32_t player_health = 0xF8; // uint32_t -> Assure que la valeur est unsigned.
     uint32_t player_ammo = 0x150;
+    uint32_t player_ammo_pistolgun = 0x13C;
     uint32_t player_kev = 0xFC;
     uint32_t player_pos_x = 0x04;
     uint32_t player_pos_y = 0x3C;
@@ -36,13 +37,16 @@ class Bypass { // Création d'une classe.
 
     ~Bypass(); // Initialisation du destructor.
 
-    bool Attach(DWORD dwPid); // DWORD : 8 Octets -> Permet de récupérer le PID.
+    bool Attach(); // DWORD : 8 Octets -> Permet de récupérer le PID.
     bool Read(uintptr_t IpBaseAddress,void *IpBuffer, SIZE_T nSize, SIZE_T *IpNumberOfBytesRead = NULL); // uintptr_t permet de prendre en paramètre un pointeur.
     bool Write(uintptr_t IpBaseAddress,void *IpBuffer, SIZE_T nSize, SIZE_T *IpNumberOfBytesWritten = NULL);
-
+    bool readDataPlayer(uintptr_t IpBaseAddress, playerData *player);
+    bool findPID();
 
     private:
     HANDLE m_hProcess = NULL;
+    DWORD pid = 0;
+    HWND hwnds;
 };
 
 Bypass::Bypass(){ // Constructor
@@ -54,8 +58,37 @@ Bypass::~Bypass(){ // Si hors du scope alors on close.
     }
 }
 
-bool Bypass::Attach(DWORD dwPid){
-    m_hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, dwPid);
+/*
+    - FindWindowA va nous retourner un handle de la fenêtre (donc trouvé).
+    Sinon FindWindowA nous retourne un NULL.
+
+*/
+
+
+bool Bypass::findPID(){
+    hwnds = FindWindowA(NULL, LPCSTR("AssaultCube"));
+    if (hwnds != NULL){
+        std::cout << "[*] - Windows find." << std::endl;
+    }
+    else{
+        std::cout << "[X] - Failure, we could not find the Windows." << std::endl;
+        system("pause");
+    }
+    GetWindowThreadProcessId(hwnds, &pid);
+    if (pid != 0){
+        std::cout << "[*] - ProcessID find." << std::endl;
+    }
+    else{
+        std::cout << "[X] - No ProcessID finds." << std::endl;
+        system("pause");
+    }
+    
+
+}
+
+
+bool Bypass::Attach(){
+    m_hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, pid);
     if (m_hProcess != NULL){
         std::cout << "[*] - Opening the process.\n" << std::endl;
         return true;
@@ -86,4 +119,25 @@ bool Bypass::Write(uintptr_t IpBaseAddress,void *IpBuffer, SIZE_T nSize, SIZE_T 
     else{
         return false;
     }
+}
+
+/*
+    - On lui donne l'handle pour qu'il puisse se rattacher à la classe.
+    - On lui donne la baseadresse .
+    - On lui dit ou stocker la valeur, ici dans une structure.
+    - On lui dit la taille de la structure (sa sera toujours un int).
+
+    - player est un pointeur pointant sur le type playerData et donc sur la structure. (-> permet d'avoir accès à une structure).
+*/
+
+
+bool Bypass::readDataPlayer(uintptr_t IpBaseAddress, playerData *player){ 
+    Read((IpBaseAddress + offsets.player_health), &player->player_health, sizeof(player->player_health));
+    Read((IpBaseAddress + offsets.player_kev), &player->player_kev, sizeof(player->player_kev));
+    Read((IpBaseAddress + offsets.player_pos_x), &player->player_pos_x, sizeof(player->player_pos_x));
+    Read((IpBaseAddress + offsets.player_pos_y), &player->player_pos_y, sizeof(player->player_pos_z));
+    Read((IpBaseAddress + offsets.player_pos_z), &player->player_pos_z, sizeof(player->player_pos_z));
+    Read((IpBaseAddress + offsets.player_pos_head), &player->player_pos_head, sizeof(player->player_pos_head));
+    Read((IpBaseAddress + offsets.player_x_mouse), &player->player_x_mouse, sizeof(player->player_x_mouse));
+    Read((IpBaseAddress + offsets.player_y_mouse), &player->player_y_mouse, sizeof(player->player_y_mouse));
 }
